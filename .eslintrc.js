4f9d27dc52eff6cd20952b216eda212df5f103a2
Pete <peteburquette@gmail.com>
2022-07-05 17:05:42 +0100

    config mods

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: "@react-native-community",
+  rules: {
+    quotes: 'double',
+    '@typescript-eslint/strict-boolean-expressions': [
+      2,
+      {
+        allowString: false,
+        allowNumber: false,,
+      },,
+    ],
+  },
 };
