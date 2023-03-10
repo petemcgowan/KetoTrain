fb30a77e1f64d223917db2008da671f25f291962
Pete <peteburquette@gmail.com>
2023-03-10 08:22:23 +0000

    adding types/eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,34 @@
 module.exports = {
-  root: true,
-  extends: "@react-native-community",
-  rules: {
-    'prettier/prettier': ['error', { singleQuote: true }]
-        "@typescript-eslint/strict-boolean-expressions": [
-      2,
-      {
-        allowString: false,
-        allowNumber: false,
-      },
+    env: {
+        browser: true,
+        es2020: true,
+        amd: true,
+        node: true,
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
     ],
-  },
-};
+    overrides: [],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    rules: {
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-filename-extension': [
+            1,
+            { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+        ],
+    },
+    settings: {
+        react: {
+            pragma: 'React',
+            version: 'detect',
+        },
+    },
+}
