9020a2388934f83a02b38149ff31f542f36aeae3
Pete <peteburquette@gmail.com>
2023-04-05 18:04:27 +0100

    app name change and firebase

--- metro.config.js
+++ metro.config.js
@@ -4,20 +4,14 @@
  *
  * @format
  */
-const defaultSourceExts =
-  require("metro-config/src/defaults/defaults").sourceExts;
+
 module.exports = {
   transformer: {
-    getTransformOptions: () => ({
+    getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
         inlineRequires: true,
       },
     }),
-  },
-  resolver: {
-    sourceExts: process.env.RN_SRC_EXT
-      ? [...process.env.RN_SRC_EXT.split(",").concat(defaultSourceExts), "cjs"] // <-- cjs added here
-      : [...defaultSourceExts, "cjs"], // <-- cjs added here
   },
 };
