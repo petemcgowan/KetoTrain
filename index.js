e3c7d7811e5ed1b572c6399ae92ec09a935e8b75
Pete <peteburquette@gmail.com>
2023-04-07 18:56:07 +0100

    android splash

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-import React from "react";
+import {AppRegistry} from 'react-native';
+import App from './src/App';
+import {name as appName} from './app.json';
 
-import {AppRegistry} from "react-native";
-// import App from "./src/App";
-import App from "./src/App";
-import {name as appName} from "./app.json";
-import {ApolloClient, InMemoryCache, ApolloProvider} from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
-  cache: new InMemoryCache(),
-});
-console.log("after Apollo client setup");
-
-// AppRegistry.registerComponent(appName, () => App);
-
-const AppWrapper = () => (
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>
-);
-
-AppRegistry.registerComponent(appName, () => AppWrapper);
+AppRegistry.registerComponent(appName, () => App);
