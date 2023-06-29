54e64ed1af941345f41d4161cc1071a03d100e51
Pete <peteburquette@gmail.com>
2023-06-29 22:41:38 +0100

    food data type (for search screen)

---
+++ client/types/FoodDataType.ts
@@ -0,0 +1,18 @@
+export type FoodDataType = {
+  food_facts_id: string
+  foodName: string
+  carbohydrates: number
+  totalDietaryFibre: number
+  protein: number
+  fatTotal: number
+  energy: number
+  totalSugars: number
+  sodium: number
+  calcium: number
+  classification: string
+  iodine: number
+  magnesium: number
+  potassium: number
+  publicFoodKey: string
+  saturatedFat: number
+}
