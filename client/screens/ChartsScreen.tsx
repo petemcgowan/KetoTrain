030e8989e7c71779564f95c846e196649bdfbb8d
Pete <peteburquette@gmail.com>
2024-07-08 19:08:38 -0500

    chart no data message

--- client/screens/ChartsScreen.tsx
+++ client/screens/ChartsScreen.tsx
@@ -1,10 +1,20 @@
 import React, { useContext } from 'react'
-import { StyleSheet, View, SafeAreaView, Text, ScrollView } from 'react-native'
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ScrollView,
+  Dimensions,
+} from 'react-native'
 import { ThemeContext } from '../state/ThemeContext'
 import EnergyChart from '../charting/EnergyChart'
 import TrackerContext from '../state/TrackerContext'
 import MacroPieChart from '../charting/MacroPieChart.tsx'
 import MacroAreaChart from '../charting/MacroAreaChart'
+import { RFPercentage } from 'react-native-responsive-fontsize'
+
+const { width, height } = Dimensions.get('screen')
 
 const ChartsScreen = () => {
   const { trackerItems } = useContext(TrackerContext)
@@ -15,14 +25,52 @@ const ChartsScreen = () => {
   const { theme } = context
   const styles = getStyles(theme)
 
+  const today = new Date()
+  const oneWeekAgo = new Date(today)
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7)
+
+  const filteredItems = trackerItems.filter(
+    (item) => new Date(item.consumptionDate) >= oneWeekAgo
+  )
+
+  const totals = filteredItems.reduce(
+    (acc, item) => {
+      acc.protein += item.proteinAmt * item.portionCount
+      acc.carb += item.carbAmt * item.portionCount
+      acc.fat += item.fatAmt * item.portionCount
+      return acc
+    },
+    {
+      protein: 0,
+      carb: 0,
+      fat: 0,
+    }
+  )
+  console.log(`totals ${JSON.stringify(totals)}`)
+
   return (
     <ScrollView style={styles.tabContainer}>
       <SafeAreaView style={styles.chartContainer}>
         <View style={styles.ketoLimitContainer}>
-          <Text style={styles.chartTitle}>
-            Nutrient consumption breakdown (last week)
-          </Text>
-          <MacroPieChart trackerItems={trackerItems} />
+          {totals.carb === 0 && totals.fat === 0 && totals.protein === 0 && (
+            <View style={styles.noDataBox}>
+              <Text style={styles.noDataText}>
+                There is no chart data for the last week
+              </Text>
+              <Text style={styles.noDataText}>
+                Add some data on the Track screen!
+              </Text>
+            </View>
+          )}
+          {(totals.carb > 0 || totals.fat > 0 || totals.protein > 0) && (
+            <View>
+              <Text style={styles.chartTitle}>
+                Nutrient consumption breakdown (last week)
+              </Text>
+              <MacroPieChart trackerItems={trackerItems} />
+            </View>
+          )}
+
           <Text style={styles.chartTitle}>Calories consumed by day</Text>
           <EnergyChart trackerItems={trackerItems} />
           <Text style={styles.chartTitle}>Nutrient breakdown by day</Text>
@@ -63,5 +111,19 @@ const getStyles = (theme) =>
       marginVertical: 20,
       width: '90%',
       alignSelf: 'center',
+    },
+    noDataBox: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      alignSelf: 'center',
+      width: width * 0.7,
+      height: height * 0.3,
+      backgroundColor: theme.viewBackground,
+    },
+    noDataText: {
+      color: theme.buttonText,
+      textAlign: 'center',
+      fontSize: RFPercentage(2.8),
     },
   })
